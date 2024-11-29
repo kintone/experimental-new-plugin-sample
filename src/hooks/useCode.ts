51862e7d0e43c7a6fd6f12e4b881e6dcc756995c
Yota Hada <hadayota33@gmail.com>
2024-11-29 15:22:40 +0900

    Fix style(#3)
    * update icon
    
    * add postcss config file
    
    * update icon and description
    
    * update icon

--- src/hooks/useCode.ts
+++ src/hooks/useCode.ts
@@ -4,7 +4,6 @@ import { EXECUTE_SHORTCUT_KEY } from "../constants";
 const defaultCode = `// Write your code here.
 // Press ${EXECUTE_SHORTCUT_KEY} or Run button to execute.
 // You can use console.log to print the output.
-// You can use kintone JavaScript API (https://cybozu.dev/ja/kintone/docs/js-api/). 
 console.log('Hello, World!');
 `;
 
